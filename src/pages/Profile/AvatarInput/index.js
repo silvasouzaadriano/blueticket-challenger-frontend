7423e0b4d6c9f7ae632c284b5f9d284a8b21deaf
Adriano R S Souza <silva.souza.adriano@gmail.com>
2019-12-22 21:03:56 -0300

    Adding IP on file url

--- src/pages/Profile/AvatarInput/index.js
+++ src/pages/Profile/AvatarInput/index.js
@@ -41,8 +41,7 @@ export default function AvatarInput() {
       <label htmlFor="avatar">
         <img
           src={
-            preview.replace('localhost', '167.172.254.115') ||
-            'https://api.adorable.io/avatars/50/abott@adorable.png'
+            preview || 'https://api.adorable.io/avatars/50/abott@adorable.png'
           }
           alt="foto_perfil"
         />
